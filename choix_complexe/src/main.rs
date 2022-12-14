8bd2d2ea76932f6cba0222e11d5b8aee35aa3ed6
Hugo Fontaine <hugo.fontaine@protonmail.com>
2022-12-14 22:18:35 +0100

    Changing program to make it able to be executed on the server

--- choix_complexe/src/main.rs
+++ choix_complexe/src/main.rs
@@ -1,26 +1,29 @@
-// 1- Read the entry
-// 2- Store entry in 2 hashsets
-// 3- Calculate the intersection of the two hashsets
-// 4- The number of movies minus the size of the intersected hashsets
+// 1- Store entry in 2 hashsets
+// 2- Calculate the intersection of the two hashsets
+// 3- The number of movies minus the size of the intersected hashsets
 
 
 use std::collections::HashSet;
-use std::fs::File;
-use std::io::{BufRead, Cursor, Read};
+use std::iter::FromIterator;
 
 fn main() {
-    // 1- Read the entry
-    let mut file= String::new();
-    File::open("entries/entry1.txt").unwrap().read_to_string(&mut file).unwrap();
-    let mut lines = Cursor::new(file).lines();
+    let mut buffer = String::new();
 
-    // 2- store entry in 2 hashmaps
-    let set_adore: HashSet<String> = HashSet::from_iter(lines.by_ref().map(|a| a.unwrap()).take(6));
-    let set_deteste : HashSet<String> = HashSet::from_iter(lines.map(|a| a.unwrap()).take(6));
+    // 1- Store entry in 2 hashsets
+    let set_adore: HashSet<String> = HashSet::from_iter((0..6).map(|_| read_line(&mut buffer).to_string()));
+    let set_deteste : HashSet<String> = HashSet::from_iter((0..6).map(|_| read_line(&mut buffer).to_string()));
 
-    // 3- Calculate the intersection of the two hashmaps
+    // 2- Calculate the intersection of the two hashmaps
     let intersection = set_adore.intersection(&set_deteste);
 
-    // 4- The number of lines in 1 hashmaps minus the size of the intersected hashmaps
+    // 3- The number of movies minus the size of the intersected hashsets
     println!("{}", 6 - intersection.count());
+}
+
+fn read_line(buffer: &mut String) -> &str {
+    buffer.clear();
+    std::io::stdin()
+        .read_line(buffer)
+        .expect("impossible to read a new line");
+    buffer.trim_end()
 }
