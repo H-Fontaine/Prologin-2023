5ee4fb3a9b7a162a1a38af2473f47aa7a99376e2
Hugo Fontaine <hugo.fontaine@protonmail.com>
2022-12-14 22:02:31 +0100

    END

--- choix_complexe/src/main.rs
+++ choix_complexe/src/main.rs
@@ -1,8 +1,26 @@
-// 1- Read and store entry in 2 hashmaps
-// 2- Calculate the intersection of the two hashmaps
-// 3- The number of lines in 1 hashmaps minus the size of the intersected hashmaps
+// 1- Read the entry
+// 2- Store entry in 2 hashsets
+// 3- Calculate the intersection of the two hashsets
+// 4- The number of movies minus the size of the intersected hashsets
 
+
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, Cursor, Read};
 
 fn main() {
-    println!("Hello, world!");
+    // 1- Read the entry
+    let mut file= String::new();
+    File::open("entries/entry1.txt").unwrap().read_to_string(&mut file).unwrap();
+    let mut lines = Cursor::new(file).lines();
+
+    // 2- store entry in 2 hashmaps
+    let set_adore: HashSet<String> = HashSet::from_iter(lines.by_ref().map(|a| a.unwrap()).take(6));
+    let set_deteste : HashSet<String> = HashSet::from_iter(lines.map(|a| a.unwrap()).take(6));
+
+    // 3- Calculate the intersection of the two hashmaps
+    let intersection = set_adore.intersection(&set_deteste);
+
+    // 4- The number of lines in 1 hashmaps minus the size of the intersected hashmaps
+    println!("{}", 6 - intersection.count());
 }
