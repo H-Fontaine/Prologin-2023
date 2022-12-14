cb331188c02c29791b8fb37df762f11cd16b40bb
Hugo Fontaine <hugo.fontaine@protonmail.com>
2022-12-14 22:52:02 +0100

    Begin of the second problem

--- mise_en_boite/src/main.rs
+++ mise_en_boite/src/main.rs
@@ -1,3 +1,55 @@
+use std::thread::current;
+
+/// * `n` - Le nombre de boîtes et de restes
+/// * `restes` - Liste des volumes des restes
+/// * `boites` - Liste des volumes des boîtes
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+
+    let n = read_line(&mut buffer)
+        .parse()
+        .expect("invalid `N` parameter");
+
+    let mut restes = read_line(&mut buffer)
+        .split_whitespace()
+        .map(str::parse)
+        .collect::<Result<_, _>>()
+        .expect("invalid `restes` parameter");
+
+    let mut boites = read_line(&mut buffer)
+        .split_whitespace()
+        .map(str::parse)
+        .collect::<Result<_, _>>()
+        .expect("invalid `boites` parameter");
+
+    //Sorting of restes and boites (sort_unstable a little faster than sort)
+    restes.sort_unstable();
+    boites.sort_unstable();
+
+    let mut restes_indice = 0;
+    let mut current_reste_size = restes[0];
+    let mut boites_indice = 0;
+    let mut res = 0;
+    while restes_indice < n && boites_indice < n {
+        if restes[restes_indice] <= boites[boites_indice] {
+            res += 1;
+            restes_indice += 1;
+            boites_indice += 1;
+            current_reste_size = restes[restes_indice];
+        } else {
+            while boites_indice < n && boites[boites_indice] < current_reste_size {
+                boites_indice += 1;
+            }
+        }
+    }
+    println!("{}", res)
+}
+
+fn read_line(buffer: &mut String) -> &str {
+    buffer.clear();
+    std::io::stdin()
+        .read_line(buffer)
+        .expect("impossible to read a new line");
+    buffer.trim_end()
 }
