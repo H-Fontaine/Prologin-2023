46df20cc5e06f5d5723c313be3cac23b6b0a7ac0
Hugo Fontaine <hugo.fontaine@protonmail.com>
2022-12-14 11:30:10 +0100

    Entries and algorithm description

--- choix_complexe/src/main.rs
+++ choix_complexe/src/main.rs
@@ -1,3 +1,8 @@
+// 1- Read and store entry in 2 hashmaps
+// 2- Calculate the intersection of the two hashmaps
+// 3- The number of lines in 1 hashmaps minus the size of the intersected hashmaps
+
+
 fn main() {
     println!("Hello, world!");
 }
