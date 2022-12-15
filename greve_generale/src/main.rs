25297abb886c187d76bbf814efe4a65005a363f4
Hugo Fontaine <hugo.fontaine@protonmail.com>
2022-12-15 20:17:43 +0100

    The whole algorithm

--- greve_generale/src/main.rs
+++ greve_generale/src/main.rs
@@ -1,8 +1,57 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 /// * `n` - le nombre de cinémas
 /// * `redirection` - le lieu de redirection de chaque cinéma
-fn trajets_retour(n: i32, redirection: Vec<i32>) {
-    for int in redirection {
-        println!("{}", int);
+fn trajets_retour(n: usize, redirection: Vec<usize>) {
+    let mut previous : Vec<Option<usize>> = vec![None; n];
+    let mut distances : Vec<Option<Rc<RefCell<usize>>>> = vec![None; n];
+    for i in 0..n {
+        if distances[i].is_none() {
+            let mut j = i;
+
+            //until we reach an already visited cinema
+            while previous[redirection[j]].is_none() {
+                previous[redirection[j]] = Some(j);
+                j = redirection[j];
+            }
+
+
+            match distances[redirection[j]].clone() {
+                Some(distance) => { //if we get to an already visited of which we already know the distance cinema we can calculate the distance easily
+                    let mut k = j;
+                    let mut new_distance = *distance.borrow() + 1;
+                    distances[k] = Some(Rc::new(RefCell::new(new_distance)));
+                    while let Some(prev) = previous[k] {
+                        new_distance += 1;
+                        distances[prev] = Some(Rc::new(RefCell::new(new_distance)));
+                        k = prev;
+                    }
+                }
+                None => { //if we get to an already visited cinema but of which we don't know the distance
+                    let knot = redirection[j];
+                    let mut k = knot;
+                    let mut cycle_size = 1;
+                    let boxed_cycle_size = Rc::new(RefCell::new(0));
+                    while redirection[k] != knot {
+                        cycle_size += 1;
+                        distances[k] = Some(boxed_cycle_size.clone());
+                        k = redirection[k];
+                    }
+                    distances[k] = Some(boxed_cycle_size.clone());
+                    *boxed_cycle_size.borrow_mut() = cycle_size;
+
+                    k = knot; //Here we are at the same point if we had gotten to an already visited cinema of which we already knew the distance
+                    let mut new_distance = cycle_size;
+                    while let Some(prev) = previous[k] {
+                        new_distance += 1;
+                        distances[prev] = Some(Rc::new(RefCell::new(new_distance)));
+                        k = prev;
+                    }
+                }
+            }
+        }
+        print!("{} ", distances[i].clone().unwrap().borrow());
     }
 }
 
@@ -13,9 +62,9 @@ fn main() {
         .parse()
         .expect("invalid `N` parameter");
 
-    let redirection: Vec<i32> = read_line(&mut buffer)
+    let redirection: Vec<usize> = read_line(&mut buffer)
         .split_whitespace()
-        .map(|a| a.parse::<i32>().expect("invalid `redirection` parameter") - 1)
+        .map(|a| a.parse::<usize>().expect("invalid `redirection` parameter") - 1)
         .collect();
 
     trajets_retour(n, redirection);
